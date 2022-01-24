73e9e1015251fd4362d2aa69b6003b1df517ff7a
Kevin Caria <kevindcaria@gmail.com>
2022-01-24 20:05:22 -0300

    recuperar y mostrar en consola que usuarios se loguean

--- src/main/java/mx/com/gm/primero/controller/ControladorInicio.java
+++ src/main/java/mx/com/gm/primero/controller/ControladorInicio.java
@@ -3,25 +3,30 @@ package mx.com.gm.primero.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.extern.slf4j.Slf4j;
 import mx.com.gm.primero.entidad.Persona;
 import mx.com.gm.primero.service.PersonaService;
 
 
 @Controller
+@Slf4j
 public class ControladorInicio {
     
     @Autowired
     private PersonaService personaService;
 
     @GetMapping("/")
-    public String inicio(Model model){
+    public String inicio(Model  model, @AuthenticationPrincipal User user){
         var personas = personaService.listarpersonas();
+        log.info("usuario que hizo login "+ user);
         model.addAttribute("personas",personas);
         return "index";
     }
